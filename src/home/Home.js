d40d8934502bf1bd64a67abb218a0651192ad050
Brenda Daroz <brenda.daroz@gmail.com>
2023-03-24 17:07:01 +0100

    add .env

--- src/home/Home.js
+++ src/home/Home.js
@@ -6,12 +6,9 @@ import { useState, useEffect } from 'react'
 function Home() {
 
   const [movies, setMovies] = useState([])
-  const API_KEY = '439baa475bab7ee26968174cbfe4007a'
-
-
 
   useEffect(() => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`)
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`)
       .then((response) => response.json())
       .then(data => {
         console.log(data.results)
